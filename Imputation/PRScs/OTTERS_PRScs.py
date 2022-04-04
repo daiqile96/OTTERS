f656e8b541c0b403d9adb8e6209a9d4c94c8d813
Qile Dai <55097180+daiqile96@users.noreply.github.com>
2022-04-04 13:40:21 -0400

    Update OTTERS_PRScs.py

--- Imputation/PRScs/OTTERS_PRScs.py
+++ Imputation/PRScs/OTTERS_PRScs.py
@@ -5,22 +5,16 @@ A TWAS method that infers posterior eQTL effect sizes under continuous shrinkage
 using eQTL summary statistics and an external LD reference panel.
 
 Usage:
-python OTTERS_PRScs.py --OTTERS_dir=PATH_TO_OTTERS --N_path=PATH_TO_N --ld_dir=PATH_TO_LD --sst_file=SUM_STATS_FILE --out_dir=OUTPUT_DIR --chrom=CHROM
+python OTTERS_PRScs.py --OTTERS_dir=PATH_TO_OTTERS --in_dir=INPUR_DIR --out_dir=OUTPUT_DIR --chrom=CHROM
                 [--window=WINDOW_SIZE --a=PARAM_A --b=PARAM_B --phi=PARAM_PHI --n_iter=MCMC_ITERATIONS --n_burnin=MCMC_BURNIN --thin=MCMC_THINNING_FACTOR --thread=THREAD --seed=SEED]
 
  - PATH_TO_OTTERS: The directory of OTTERS source code
 
- - PATH_TO_N: Full path and the file name of the gene sample size file.
+ - INPUT_DIR:  Full path and the file name of the LD reference file.
 
- - PATH_TO_LD:  Full path and the file name of the LD reference file.
-
- - SUM_STATS_FILE: Full path and the file name of the summary statistics.
-
- - OUTPUT_DIR: Output directory and output filename prefix of the posterior effect size estimates.
+ - OUTPUT_DIR: Output directory and output filename of the posterior effect size estimates.
 
  - CHROM: An integer for the chromosome of tested genes.
-
- - WINDOW (optional): Window size (in base pairs) around gene region from which to include SNPs (default: 1000000 [+- 1MB region around gene region])
 
  - PARAM_A (optional): Parameter a in the gamma-gamma prior in the continous shrinkage prior proposed by PRS-CS. Default is 1. 
 
@@ -47,15 +41,13 @@ import multiprocessing
 import subprocess
 import getopt
 import sys
-
-from io import StringIO
-from time import time
-from numpy import linalg
-
 import os
+
 import numpy as np
 import pandas as pd
 
+from time import time
+from numpy import linalg
 
 import mcmc_gtb
 
@@ -67,13 +59,13 @@ start_time = time()
 
 
 def parse_param():
-    long_opts_list = ['OTTERS_dir=', 'N_path=', 'ld_dir=', 'sst_dir=',
+    long_opts_list = ['OTTERS_dir=', 'in_dir=',
                       'out_dir=', 'chrom=', 'window=', 'thread=',
                       'a=', 'b=', 'phi=', 'n_iter=', 'n_burnin=', 'thin=',
                       'seed=', 'help']
 
-    param_dict = {'OTTERS_dir': None, 'N_path': None, 'ld_dir': None,
-                  'sst_dir': None, 'out_dir': None, 'chrom': None,
+    param_dict = {'OTTERS_dir': None, 'in_dir': None,
+                  'out_dir': None, 'chrom': None,
                   'window': 1000000, 'thread': 1, 'a': 1, 'b': 0.5,
                   'phi': None, 'n_iter': 1000, 'n_burnin': 500, 'thin': 5,
                   'seed': None}
@@ -94,12 +86,8 @@ def parse_param():
                 sys.exit(0)
             elif opt == "--OTTERS_dir":
                 param_dict['OTTERS_dir'] = arg
-            elif opt == "--N_path":
-                param_dict['N_path'] = arg
-            elif opt == "--ld_dir":
-                param_dict['ld_dir'] = arg
-            elif opt == "--sst_dir":
-                param_dict['sst_dir'] = arg
+            elif opt == "--in_dir":
+                param_dict['in_dir'] = arg
             elif opt == "--out_dir":
                 param_dict['out_dir'] = arg
             elif opt == "--chrom":
@@ -131,14 +119,8 @@ def parse_param():
     if param_dict['OTTERS_dir'] is None:
         print('* Please specify the directory to OTTERS --OTTERS_dir\n')
         sys.exit(2)
-    elif param_dict['N_path'] is None:
+    elif param_dict['in_dir'] is None:
         print('* Please specify the full path to the sample size file using --anno_dir\n')
-        sys.exit(2)
-    elif param_dict['ld_dir'] is None:
-        print('* Please specify the directory to the LD reference file using --ld_dir\n')
-        sys.exit(2)
-    elif param_dict['sst_dir'] is None:
-        print('* Please specify the eQTL summary statistics file using --sst_dir\n')
         sys.exit(2)
     elif param_dict['chrom'] is None:
         print('* Please specify the chromosome --chrom\n')
@@ -160,70 +142,117 @@ param_dict = parse_param()
 sys.path.append(param_dict['OTTERS_dir'])
 import OTTERSutils as ots
 
+chrom = param_dict['chrom']
+input_dir = param_dict['in_dir']
+
 print('Reading sample size file.\n')
 N_chunks = pd.read_csv(
-    param_dict['N_path'],
+    os.path.join(input_dir, 'medianN.txt'),
     sep='\t',
     header=0,
     chunksize=10000,
     iterator=True,
-    dtype={'TargetID': object, 'N': np.int32})
+    dtype={'CHROM': object, 'TargetID': object, 'N': np.int32})
 
-GeneN = pd.concat([x[x['CHROM'] == param_dict['chrom']] for x in N_chunks]).reset_index(drop=True)
+GeneN = pd.concat([x[x['CHROM'] == chrom] for x in N_chunks]).reset_index(drop=True)
 
 if GeneN.empty:
-    raise SystemExit('There are no valid sample size data for chromosome ' + param_dict['chrom'] + '\n')
+    raise SystemExit('There are no valid sample size data for chromosome ' + chrom + '\n')
 
 TargetID = GeneN.TargetID
 n_targets = TargetID.size
 N = GeneN.N
 
+# create output file for PRS-CS
+out_cols = ['CHROM', 'POS', 'A1', 'A2', 'TargetID', 'ES']
+pd.DataFrame(columns=out_cols).to_csv(
+    param_dict['out_dir'],
+    sep='\t',
+    index=None,
+    header=True,
+    mode='w')
 
-############################################################
+# ############################################################
 
 @ots.error_handler
 def thread_process(num):
     print('Reading eQTL summary statistics data.')
+
     target = TargetID[num]
-    target_n = N[num]
     print('num=' + str(num) + '\nTargetID=' + target)
+    target_n = N[num]
+    target_dir = os.path.join(input_dir, target)
 
-    # extract summary statistics and reference LD matrix for the gene
+    # extract summary statistics 
     print('Extracting eQTL summary statistics data.')
-
-    sst_path = os.path.join(param_dict['sst_dir'], target, target+'_beta.txt')
-
+    sst_path = os.path.join(target_dir, target + '_beta.txt')
     sst_chunks = pd.read_csv(sst_path, sep='\t',
                              low_memory=False,
-                             header=None,
-                             names=['SNP', "A1", "A2", "Beta"],
-                             iterator=True, chunksize=1000)
-
+                             header=0,
+                             names=['SNP', "A1", "A2", "BETA"],
+                             iterator=True, chunksize=1000,
+                             dtype={'SNP': object, 'A1': object, 'A2': object, 'BETA': np.float32})
     target_sst = pd.concat([chunk for chunk in sst_chunks]).reset_index(drop=True)
 
     if len(target_sst) == 0:
-        print('There is no summary statistics data for TargetID: ' + target + '\n')
+        print('There is no summary statistics data for TargetID: ' + target)
+        print('Please use OTTERS to prepare input files \n')
         return None
 
-    print('Extracting reference LD matrix.')
+    # Save the list of SNPs that are kept in the summary statistics 
+    target_snplist = target_sst['SNP']
+    target_snplist_path = os.path.join(target_dir, target + '.snplist')
 
-    # construct covariance matrix
-    snp_search_ids = target_sst['SNP']
-    ld_blk = ots.get_ld_data(param_dict['ld_dir'], snp_search_ids)
+    target_snplist.to_csv(target_snplist_path,
+                          sep='\t',
+                          index=None,
+                          header=None,
+                          mode='w')
 
-    if len(target_sst['SNP']) > len(ld_blk['SNP']):
-        print('%d eQTLs does not have genotype data in LD reference panel.' % (len(target_sst['SNP'] not in (ld_blk['SNP']))))
-        print('Please consider use OTTERS to prepare summary statistics')
-        target_sst = target_sst[target_sst['SNP'].isin(ld_blk['SNP'])]
+    # Calculate LD for these SNPs.
+    print('Generating reference LD matrix.')
+    snplist = target + '.snplist'
+    get_ld_cmd = ["plink --bfile " + target + " --keep-allele-order --extract " +
+                  snplist + " --r square --out " + target + " --memory 2000 "]
 
-    blk_size, V_cov = ots.get_ld_matrix(ld_blk)
+    try:
+        ld_proc = subprocess.check_call(get_ld_cmd,
+                                    stdout=subprocess.PIPE,
+                                    cwd=target_dir,
+                                    shell=True,
+                                    bufsize=1)
+        print('LD calculation completed')
+    except subprocess.CalledProcessError as err:
+        print('calculation failed for TargetID: ' + target + '\n')
+        return None
+
+    target_ld = os.path.join(target_dir, target + '.ld')
+
+    ld_chunks = pd.read_csv(target_ld, sep='\t',
+                            low_memory=False,
+                            header=None,
+                            iterator=True,
+                            chunksize=1000)
+
+    ld = pd.concat([chunk for chunk in ld_chunks]).reset_index(drop=True)
+
+    # Make sure the ld is positive definite matrix
+    # PLINK rounds to 6 decimal points, which sometimes makes the correlation matrix become not positive definite
+    V = ld.to_numpy()
+    # PLINK return nan when all mutually-nonmissing set is homozygous major
+    # We set nan = 0 here 
+    V = np.nan_to_num(V)
+    _, s, v = linalg.svd(V)
+    h = np.dot(v.T, np.dot(np.diag(s), v))
+    V = (V+h)/2
+    blk_size = len(V)
 
     if blk_size > 0:
         try:
             target_sst['ES'] = mcmc_gtb.mcmc(a=param_dict['a'], b=param_dict['b'],
                                              phi=param_dict['phi'],
                                              sst_dict=target_sst, n=target_n,
-                                             ld_blk=V_cov,
+                                             ld_blk=V,
                                              blk_size=blk_size,
                                              n_iter=param_dict['n_iter'],
                                              n_burnin=param_dict['n_burnin'],
@@ -233,14 +262,10 @@ def thread_process(num):
             print('PRS-CS failed for TargetID: ' + target + '\n')
             return None
 
-    if param_dict['phi'] is None:
-        target_weights_path = param_dict['out_dir'] + 'eQTL_weights_a%d_b%.1f_phiauto_chr%d.txt' % (param_dict['a'], param_dict['b'], int(param_dict['chrom']))
-    else:
-        target_weights_path = param_dict['out_dir'] + 'eQTL_weights_a%d_b%.1f_phi%1.0e_chr%d.txt' % (param_dict['a'], param_dict['b'], param_dict['phi'], int(param_dict['chrom']))
-
-    with open(target_weights_path, 'a') as ff:
-        for pos, a1, a2, beta in zip(target_sst['SNPPos'], target_sst['A1'], target_sst['A2'], target_sst['ES']):
-            ff.write('%d\t%d\t%s\t%s\t%s\t%.6e\n' % (int(param_dict['chrom']), int(pos), a1, a2, target, beta))
+    target_sst['POS'] = [int(snp.split('_')[1]) for snp in target_sst['SNP']]
+    with open(param_dict['out_dir'], 'a') as ff:
+        for pos, a1, a2, beta in zip(target_sst['POS'], target_sst['A1'], target_sst['A2'], target_sst['ES']):
+            ff.write('%d\t%d\t%s\t%s\t%s\t%.6e\n' % (int(chrom), int(pos), a1, a2, target, beta))
 
     print('Done ' + str(num) + ':' + target + '\n')
 
